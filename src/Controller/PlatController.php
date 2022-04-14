1738f2dce59ded487755c1bfac31bdf12e0b7c7e
OussemaChaabene <99026165+OussemaChaabene@users.noreply.github.com>
2022-04-14 12:18:46 +0200

    controller

--- src/Controller/PlatController.php
+++ src/Controller/PlatController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Plat;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Form\PlatType;
+use Symfony\Component\HttpFoundation\Request;
 class PlatController extends AbstractController
 {
     /**
@@ -17,6 +19,26 @@ class PlatController extends AbstractController
             'controller_name' => 'PlatController',
         ]);
     }
+    /**
+     * @Route("/addPlat", name="addPlat")
+     */
+    public function addPlat(Request $request): Response
+    {
+        $plat = new Plat();
 
+        $form = $this->createForm(PlatType::class,$plat);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($plat);
+            $em->flush();
+
+            return $this->redirectToRoute('app_plat');
+        }
+        return $this->render('plat/createPlat.html.twig',['f'=>$form->createView()]);
+
+    }
 
 }
---
+++ src/Form/PlatType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Plat;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+class PlatType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('poids')
+            ->add('sodium')
+            ->add('cholesterol')
+            ->add('carbohydrate')
+            ->add('protein')
+            ->add('calories')
+            ->add('regimes')
+            ->add('Ajouter', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Plat::class,
+        ]);
+    }
+}
